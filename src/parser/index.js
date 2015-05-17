a475a49c245d9a2b6ab48750682d0b55ad29e992
Braulio Valdivielso Martínez <brlvldvlsmrtnz@gmail.com>
2015-05-17 20:59:06 +0200

    Refactored parser/index.js and added tests for parseRule

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,19 +1,17 @@
 import Machine, {LEFT, RIGHT} from '../machine';
 
-function notComment(line){
-	return line[0] !== "#";
+export class SyntaxError extends Error {
+	constructor(message, line) {
+		super();
+		this.message = message;
+		this.line = line;
+	}
 }
 
-function notEmpty(line){
-	return line.length !== 0;
-}
-
-function parseRule(line){
-	let [condition, transition] = line.split("=>");
-	let [originState, readSymbol] = condition.split(" ").filter(notEmpty);
-	let [nextState, symbolToWrite, direction] = transition.split(" ").filter(notEmpty);
-
-	direction = direction == "RIGHT" ? RIGHT : LEFT;
+export function parseRule(line){
+	let [condition, transition] = extractRuleParts(line);
+	let [originState, readSymbol] = extractCondition(condition);
+	let [nextState, symbolToWrite, direction] = extractTransition(transition);
 
 	return [originState, readSymbol, nextState, symbolToWrite, direction];
 }
@@ -32,3 +30,59 @@ export default function parse(code){
 
 	return machine;
 }
+
+function notComment(line){
+	return line[0] !== "#";
+}
+
+function notEmpty(line){
+	return line.length !== 0;
+}
+
+function extractRuleParts(rule){
+	let ruleParts = rule.split("=>");
+
+	if (ruleParts.length !== 2){
+		throw new SyntaxError("Instruction error", line);
+	}
+
+	return ruleParts;
+}
+
+function extractCondition(conditionPart) {
+	let conditionParts = conditionPart.split(" ").filter(notEmpty);
+
+	if (conditionParts.length !== 2) {
+		throw new SyntaxError("Rule Condition Error", line);
+	}
+
+	return conditionParts;
+}
+
+
+function extractTransition(transitionPart){
+	let transitionParts = transitionPart.split(" ").filter(notEmpty);
+
+	if (transitionParts.length !== 3){
+		throw new SyntaxError("Rule transition Error", transitionPart);
+	}
+
+	transitionParts[2] = extractDirection(transitionParts[2]);
+
+	return transitionParts;
+}
+
+function extractDirection(direction){
+	if (direction === "LEFT") {
+		return LEFT;
+	} else if (direction === "RIGHT") {
+		return RIGHT;
+	}
+
+	throw new SyntaxError("Rule direction error", direction);
+}
+
+
+
+
+
--- test/parser/index.js
+++ test/parser/index.js
@@ -1,12 +1,113 @@
 var parse = require("../../lib/parser");
 var should = require("should");
 
-describe("The parser", function(){
+var parseRule = parse.parseRule;
+var RIGHT = 1;
+var LEFT = -1;
 
-	it.skip("should ignore comments", function(){
-		
+describe("Parser module", function(){
+
+	describe("The rule parser", function(){
+		it("should parse properly basic rules", function(){
+			should.deepEqual(parseRule("Q0 0 => Q1 1 RIGHT"), [
+				"Q0",
+				"0",
+				"Q1",
+				"1",
+				RIGHT
+			]);
+
+			should.deepEqual(parseRule("Q1 1 => Q102 0 LEFT"), [
+				"Q1",
+				"1",
+				"Q102",
+				"0",
+				LEFT
+			]);
+		});
+
+		it("should not care about whitespaces", function(){
+			should.deepEqual(parseRule(" Q0    0 =>Q1  1 RIGHT "), [
+				"Q0",
+				"0",
+				"Q1",
+				"1",
+				RIGHT
+			]);
+
+			should.deepEqual(parseRule(" Q0 0=>Q1  1     LEFT"), [
+				"Q0",
+				"0",
+				"Q1",
+				"1",
+				LEFT
+			]);
+		});
+
+		it("should throw if the instruction was not valid", function(){
+			(function(){
+				parseRule("Q0 0 = Q1 1 LEFT");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 => Q1 1 LEFT =>");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 <= Q1 1 LEFT");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 Q1 1 LEFT");
+			}).should.throw();
+
+		});
+
+		it("should throw if the condition was not valid", function(){
+			(function(){
+				parseRule("Q0 0 1 => Q1 1 LEFT");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 => Q1 1 LEFT");
+			}).should.throw();
+
+			(function(){
+				parseRule("=> Q1 1 LEFT");
+			}).should.throw();
+		});
+
+		it("should throw if the transition was not valid", function(){
+			(function(){
+				parseRule("Q0 0 => Q1 1 LEFT HEHE");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 => Q1 1");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 => Q1");
+			}).should.throw();
+		});
+
+		it("should throw if the direction was not ok", function(){
+			(function(){
+				parseRule("Q0 0 => Q1 1 IZQUIERDA");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 => Q1 0 left");
+			}).should.throw();
+
+			(function(){
+				parseRule("Q0 0 => Q1 0 WRIGHT");
+			}).should.throw();
+		});
+
 
 	});
+	
 
 
 });
